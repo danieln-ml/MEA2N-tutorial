9ecd138c64b7fcc67b2f3a4a00f31d3e4fe6f119
Daniel Nakashima <daniel@objectlabs.com>
2016-11-21 15:58:24 -0800

    showing list of mocked out users

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Http, Response} from '@angular/http';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
+
+
 @Component({
-  selector: 'app-contact-list',
+  selector: 'contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
+  providers: [ContactService]
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  title: 'Contacts List'
+  contacts: Contact[]
+  selectedContact: Contact
+  loaded: boolean
+
+  constructor (private contactService: ContactService) {}
 
   ngOnInit() {
+    this.contactService.getContacts().then((contacts: Contact[]) => {
+      this.contacts = contacts
+    });
   }
 
 }
--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { CONTACTS, CONTACT } from './mock-contacts';
+import { Observable }     from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { Contact } from './contact';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
-export class ContactServiceService {
+export class ContactService {
 
   constructor() { }
+
+  // get("/contacts")
+  getContacts(): Promise<Contact[]> {
+    return Promise.resolve(CONTACTS);
+  }
 
 }
