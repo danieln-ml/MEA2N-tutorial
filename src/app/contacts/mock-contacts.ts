195684b8410d13fcb1343b255d2cd3d7a82cac40
Daniel Nakashima <daniel@objectlabs.com>
2016-11-21 15:36:25 -0800

    rename of files and added mocks

--- src/app/contacts/mock-contacts.ts
+++ src/app/contacts/mock-contacts.ts
@@ -1,2 +1,70 @@
-export class MockContacts {
+import { Contact } from './contact';
+
+export const CONTACTS: Contact[] = [
+  {
+    _id: 11,
+    name: 'Mr.Nice',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 12,
+    name: 'Narco',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 13,
+    name: 'Bombasto',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 14,
+    name: 'Celeritas',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 15,
+    name: 'Magneta',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 16,
+    name: 'RubberMan',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 17,
+    name: 'Dynama',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 18,
+    name: 'Dr IQ',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 19,
+    name: 'Magma',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  },
+  {
+    _id: 20,
+    name: 'Tornado',
+    email: "example@example.com",
+    phone: { mobile: 'mobile', work: 'worknum'}
+  }
+];
+export const CONTACT: Contact = {
+  _id: 11,
+  name: 'example name',
+  email: "example@example.com",
+  phone: { mobile: 'mobile example', work: 'work example'}
 }
