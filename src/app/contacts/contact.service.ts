a590e26f0de94daa6accfb859284f855bbdd2bc4
Daniel Nakashima <daniel@objectlabs.com>
2016-12-04 14:08:29 -0800

    using backend instead of mocks

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CONTACTS} from './mock-contacts';
+import { CONTACTS, CONTACT} from './mock-contacts';
 import { Contact } from './contact';
+import { Observable }     from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ContactService {
+  // URL to web API
+  private contactsUrl = '/api/contacts';
 
-  constructor() { }
+  constructor (private http: Http) {}
 
   // get("/contacts")
   getContacts(): Promise<Contact[]> {
-    return Promise.resolve(CONTACTS);
+    return this.http.get(this.contactsUrl)
+               .toPromise()
+               .then(response => response.json() as Contact[])
+               .catch(this.handleError);
+  }
+
+  updateContact(contact: Contact): Promise<Contact> {
+    return Promise.resolve(contact);
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
   }
 }
