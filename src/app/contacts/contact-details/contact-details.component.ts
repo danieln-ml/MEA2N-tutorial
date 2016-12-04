a3d2a59d5d02657fb5576bf952e4c8e727925cbd
Daniel Nakashima <daniel@objectlabs.com>
2016-12-04 14:08:18 -0800

    add contact detail-master component to app

--- src/app/contacts/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
 
 @Component({
-  selector: 'app-contact-details',
-  templateUrl: './contact-details.component.html',
-  styleUrls: ['./contact-details.component.css']
+  selector: 'contact-detail',
+  template: `
+    <div *ngIf="contact">
+      <h2 *ngIf="contact._id">Contact Details</h2>
+      <h2 *ngIf="!contact._id">Create Contact</h2>
+      <form>
+        <div class="form-group">
+          <label for="contact-name">Name:</label>
+          <input class="form-control" name="contact-name" [(ngModel)]="contact.name" placeholder="Name"/>
+        </div>
+        <div class="form-group">
+          <label for="contact-email">Email:</label>
+          <input class="form-control" name="contact-email" [(ngModel)]="contact.email" placeholder="example@abc.com"/>
+        </div>
+        <div class="form-group">
+          <label for="contact-phone-mobile">Mobile:</label>
+          <input class="form-control" name="contact-phone-mobile" [(ngModel)]="contact.phone.mobile" placeholder="6431111"/>
+        </div>
+        <div class="form-group">
+          <label for="contact-phone-work">Work:</label>
+          <input class="form-control" name="contact-phone-work" [(ngModel)]="contact.phone.work" placeholder="9670309"/>
+        </div>
+      </form>
+    </div>
+  `
 })
-export class ContactDetailsComponent implements OnInit {
+export class ContactDetailsComponent {
+  @Input()
+  contact: Contact;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor (private contactService: ContactService) {}
 }
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact';
 import { ContactService } from '../contact.service';
-
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
 
 @Component({
   selector: 'contact-list',
@@ -12,12 +12,21 @@ import { ContactService } from '../contact.service';
 export class ContactListComponent implements OnInit {
 
   contacts: Contact[]
+  selectedContact: Contact
 
   constructor (private contactService: ContactService) {}
 
   ngOnInit() {
+    this.getContacts();
+  }
+
+  getContacts() {
     this.contactService.getContacts().then((contacts: Contact[]) => {
       this.contacts = contacts
     });
+  }
+
+  selectContact(contact: Contact) {
+    this.selectedContact = contact
   }
 }
